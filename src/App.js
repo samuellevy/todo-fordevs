a78b8f663eb680c71f34d8d9e4b5f7f04760db63
Cayque Freitas <cayque.freitas@3aworldwide.com.br>
2019-05-03 12:16:36 -0300

    react front

--- src/App.js
+++ src/App.js
@@ -5,20 +5,62 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <article>
+            <div class='header'>
+                <h1>Concluir tarefa do Electron</h1>
+                <span class='tag'>electron</span>
+                <span class='tag'>php</span>
+                <span class='tag'>js</span>
+                <span class='tag'>react</span>
+            </div>
+            <div class="content">
+                <p>buscar dados suficientes para enrolar qualquer texto aqui.</p>
+            </div>
+            <div class="footer">
+                <div>13 de abril</div>
+                <div>05 de maio</div>
+            </div>
+
+        </article>
+        
+        <article>
+            <div class='header'>
+                <h1>Concluir tarefa do Electron</h1>
+                <span class='tag'>electron</span>
+                <span class='tag'>php</span>
+                <span class='tag'>js</span>
+                <span class='tag'>react</span>
+            </div>
+            <div class="content">
+                <p>buscar dados suficientes para enrolar qualquer texto aqui.</p>
+            </div>
+            <div class="footer">
+                <div>13 de abril</div>
+                <div>05 de maio</div>
+            </div>
+
+        </article>
+        
+        <article>
+            <div class='header'>
+                <h1>Concluir tarefa do Electron</h1>
+                <span class='tag'>electron</span>
+                <span class='tag'>php</span>
+                <span class='tag'>js</span>
+                <span class='tag'>react</span>
+            </div>
+            <div class="content">
+                <p>buscar dados suficientes para enrolar qualquer texto aqui.</p>
+            </div>
+            <div class="footer">
+                <div>13 de abril</div>
+                <div>05 de maio</div>
+            </div>
+
+        </article>
+        
+    </section>
     </div>
   );
 }
