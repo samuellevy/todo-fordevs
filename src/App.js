248266cdec9b703faf407a98b0627ab29ba02759
Samuel Levy <samuellevy@MacBook-Pro-de-Samuel.local>
2019-05-04 19:01:38 -0300

    setting up live reload on electron and reactjs, starting menu block

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFile,faFolder,faPen,faTrash,faCloud } from '@fortawesome/free-solid-svg-icons'
+
 import ItemBox from './components/ItemBox';
+
+library.add(faFile,faFolder,faPen,faTrash,faCloud);
 
 function App() {
   return (
     <div className="App">
-        <section>
-            <ItemBox/>
-        </section>
+      <aside>
+        <li><FontAwesomeIcon icon="file" className="icon" size="2x"/></li>
+        <li><FontAwesomeIcon icon="folder" className="icon" size="2x"/></li>
+        <li><FontAwesomeIcon icon="pen" className="icon" size="2x"/></li>
+        <li><FontAwesomeIcon icon="trash" className="icon" size="2x"/></li>
+        <li><FontAwesomeIcon icon="cloud" className="icon" size="2x"/></li>
+      </aside>
+      <section>
+        <ItemBox/>
+        <ItemBox/>
+        <ItemBox/>
+        <ItemBox/>
+        <ItemBox/>
+        <ItemBox/>
+        <ItemBox/>
+      </section>
     </div>
   );
 }
