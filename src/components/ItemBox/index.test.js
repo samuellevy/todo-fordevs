e97b55f5c7bdce5c3616814ff4955763ce901952
Samuel Levy <samuellevy@MacBook-Pro-de-Samuel.local>
2019-05-05 14:15:34 -0300

    organizing tests

--- src/components/ItemBox/index.test.js
+++ src/components/ItemBox/index.test.js
@@ -7,11 +7,3 @@ it('renders without crashing', () => {
     ReactDOM.render(<ItemBox />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
-
-
-describe('Examining the syntax of Jest tests', () => {
-    it('sums numbers', () => {
-        expect(1 + 2).toEqual(3);
-        expect(2 + 2).toEqual(4);
-    });
-});
